91c817b524c605153ebed81d9b3c74763dea544a
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-10 16:24:06 +0200

    feat: Add a timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+
+export default function Timer({active = true}:{active: boolean}) {
+    const [startTime, setStartTime] = useState<null | Date>(null);
+    const [currentTime, setCurrentTime] = useState<string>("");    
+
+    useEffect(() => {
+        setStartTime(new Date());
+        setCurrentTime(printTime(0));
+    }, []);
+    
+    const printTime = (time: number) => {
+        const timeInSeconds = Math.trunc(time / 1000);
+        const timeMinutes = Math.trunc(timeInSeconds / 60);
+        const timeSecs = Math.trunc(timeInSeconds % 60);
+        return `${timeMinutes}m ${timeSecs}s`
+    }
+
+    setInterval(() => {
+        if (startTime !== null && active) {
+            const newTime = printTime(new Date().getTime() - startTime.getTime())
+            setCurrentTime(newTime);
+        }
+    }, 100);
+    
+    
+    return (currentTime &&
+        <p>
+            {currentTime}
+        </p>
+    )
+
+}
