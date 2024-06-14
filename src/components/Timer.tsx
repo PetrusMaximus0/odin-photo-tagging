024f1187596033a13b10484333bd8934e454c60c
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-15 01:44:40 +0200

    feat: active prop for Timer component is made optional.

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
 
-export default function Timer({active = true}:{active: boolean}) {
+export default function Timer({active=true}:{ active?: boolean}) {
     const [startTime, setStartTime] = useState<null | Date>(null);
-    const [currentTime, setCurrentTime] = useState<string>("");    
+    const [currentTime, setCurrentTime] = useState<string>("");
 
     useEffect(() => {
         setStartTime(new Date());
         setCurrentTime(printTime(0));
-    }, []);
+    },[]);
     
     const printTime = (time: number) => {
         const timeInSeconds = Math.trunc(time / 1000);
@@ -16,14 +16,12 @@ export default function Timer({active = true}:{active: boolean}) {
         const timeSecs = Math.trunc(timeInSeconds % 60);
         return `${timeMinutes}m ${timeSecs}s`
     }
-
     setInterval(() => {
-        if (startTime !== null && active) {
+        if (startTime !== null && active === true) {
             const newTime = printTime(new Date().getTime() - startTime.getTime())
             setCurrentTime(newTime);
         }
     }, 100);
-    
     
     return (currentTime &&
         <p>
