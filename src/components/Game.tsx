c364f4825cfab425a0eb4249fc063ec94be73e1e
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-15 18:29:11 +0200

    refactor: Improve code readability and structure. Feat: Add cancelGameSession function.
    - cancelGameSession requests removal of ongoing incomplete sessions from the database.
    
    - Move closeGameSession callback outside of useEffect. There is no need to keep it inside useEffect. It should be called on a condition and not on re-renders.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,19 +1,22 @@
-//
+// Components imported
 import Timer from "./Timer";
 import Circle from "./Circle";
+import SaveUsername from "./SaveUsername";
+
 //
 import React, { useEffect, useRef, useState } from "react";
-//
+import { useNavigate } from "react-router-dom";
+
+// Image resources
 import gameSpace from "../assets/game_space.jpeg"
 import waldoProfile from "../assets/waldo_space.png"
 import yeldoProfile from "../assets/yeldo_space.png"
 import wizzardProfile from "../assets/wizzard_space.png"
-import { useNavigate } from "react-router-dom";
-import SaveUsername from "./SaveUsername";
 
+// Import the apiURl
 const apiURL = import.meta.env.VITE_API_URL;
 
-export default function Game() {
+export default function Game({boxSize = 16}: {boxSize?: number}) {
     //
     const imageRef = useRef<null | HTMLElement>(null);
     
@@ -28,19 +31,14 @@ export default function Game() {
     }
 
     //
-    interface ISession{
-        _id: "string"
-    }
-
-    //
-    const [gameState, setGameState] = useState<"ongoing" | "endTop10" | "end">("ongoing");
+    const [gameState, setGameState] = useState<"ongoing" | "end">("ongoing");
 
     //
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<Error | null>(null);
     //
     const [score, setScore] = useState<{time: number, id: string}>({time: 0, id: ""});
-    const [session, setSession] = useState<ISession | null>(null);
+    const [session, setSession] = useState<{_id: string}| null>(null);
     const [solutions, setSolutions] = useState<ISolutions | null>(null)
     const [foundCharacters, setFoundCharacters] = useState({ waldo: false, yeldo: false, wizzard: false });
     //
@@ -48,7 +46,55 @@ export default function Game() {
     const [showMarker, setShowMarker] = useState(false);
     const [markerRelativePosition, setMarkerRelativePosition] = useState({ x: 0, y: 0 });
 
-    //
+    // When the user clicks on the game screen...
+    // Handle the clicking on the screen based on which elementName is passed to the function
+    const handleSelectClick = async (e: React.MouseEvent<HTMLElement>, elementName: string) => {
+        e.stopPropagation();
+        if (elementName === "image") {
+            setMarker(e);
+        
+        } else {
+            // Check if we clicked on a character and correctly identified it.
+            const response = await isCharacter(elementName);
+            
+            if (response.result === true) {
+                // Character was identified correctly
+                const newFoundCharacters = { ...foundCharacters, [`${elementName}`]: true }
+                setFoundCharacters(newFoundCharacters);
+                //
+                setSolutions({ ...solutions, [`${response.character}`]: response.solution })
+
+                // Check if game is over
+                if (newFoundCharacters.waldo && newFoundCharacters.yeldo && newFoundCharacters.wizzard) {
+                    // The game is over, close the session and change game state.
+                    closeGameSession();
+
+                    // Scroll the window back up so the player sees their score.
+                    window.scrollTo(0, 0);
+                }
+            }
+            setShowMarker(false);            
+        }
+    }
+
+    // Sets the marker on the screen according to the player's click location, or unsets when clicked again.
+    const setMarker = (e :React.MouseEvent<HTMLElement>) => {
+        const element = e.target as HTMLElement;
+
+        if (element.classList.contains("marker")) { 
+            // If we click on the marker that is already set, unset it
+            setShowMarker(false);
+            
+        } else {
+            // Set a new marker according to the mouse coordinates.
+            const rect = e.currentTarget.getBoundingClientRect();
+            setShowMarker(true);
+            setMarkerRelativePosition({ x: (e.clientX - rect.left) / e.currentTarget.offsetWidth, y: (e.clientY - rect.top) / e.currentTarget.offsetHeight });
+            setMarkerPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+        }
+    }
+
+    // Make a request to check if the coordinates clicked correspond to the selected character's coordinates.
     const isCharacter = async (character: string) => {
         try {
             const response = await fetch(`${apiURL}/game/verifyClick`, {
@@ -69,95 +115,46 @@ export default function Game() {
         } catch (error) {
             console.error(error);
             return false;
-        }
-        
+        }        
     }
 
-    //
-    const setMarker = (e :React.MouseEvent<HTMLElement>) => {
-        const element = e.target as HTMLElement;
-
-        if (element.classList.contains("marker")) {            
-            setShowMarker(false);
+    // Requests a session close to the server and sets the local game state to "end".
+    const closeGameSession = async () => {
+        try {
+            const response = await fetch(apiURL + "/session/close", {
+                mode: "cors",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({id: session!._id})
+            })
             
-        } else {
-            const rect = e.currentTarget.getBoundingClientRect();
-            setShowMarker(true);
-            setMarkerRelativePosition({ x: (e.clientX - rect.left) / e.currentTarget.offsetWidth, y: (e.clientY - rect.top) / e.currentTarget.offsetHeight });
-            setMarkerPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
-        }
-    }
-       
-    //
-    const handleSelectClick = async (e: React.MouseEvent<HTMLElement>, elementName: string) => {
-        e.stopPropagation();
-        if (elementName === "image") {
-            setMarker(e);
-        
-        } else {
-            
-            const response = await isCharacter(elementName);
-            
-            if (response.result === true) {
-                //
-                const newFoundCharacters = { ...foundCharacters, [`${elementName}`]: true }
-                setFoundCharacters(newFoundCharacters);
-                //
-                setSolutions({ ...solutions, [`${response.character}`]: response.solution })
-
+            if (response.status >= 400) {
+                throw new Error("An error has ocurred while attemping to close end Game");
             }
-            setShowMarker(false);            
-        }
 
-    }
+            const result = await response.json();
 
-    //
-    useEffect(() => {
-        //
-        const endGame = async () => {
-            try {
-                const response = await fetch(apiURL + "/session/close", {
-                    mode: "cors",
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({id: session!._id})
-                })
-                
-                if (response.status >= 400) {
-                    throw new Error("An error has ocurred while attemping to close end Game");
-                }
-
-                const result = await response.json();
-
-                if (result.score !== null) {
-                    // Handle the score is in the top 10
-                    setGameState("endTop10");
-
-                } else {
-                    // Handle the score is NOT in the top 10
-                    setGameState("end");
-                }
+            if (result.score !== null) {
+                // Received a score object, set the game to the end stage and store the score.
+                setGameState("end");
                 setScore({time: result.totalTime, id: result._id});
-                
-            } catch (error) {
-                setError(error as Error);
+            } else {
+                throw new Error("Failed retrieving score from server.")
             }
-        }
-        
-        //
-        if (foundCharacters.waldo && foundCharacters.yeldo && foundCharacters.wizzard) {
-            endGame();
-            //navigate("/leaderboard");
 
+            
+        } catch (error) {
+            setError(error as Error);
         }
-    }, [session, foundCharacters, navigate])
+    }
 
+    // On the page initial load...
     // Start a game session
     useEffect(() => {
         // When the component is loaded, request a game session start
-        const startGame = async () => {
+        const startGameSession = async () => {
             try {
                 setGameState("ongoing");
                 const response = await fetch(apiURL + "/session/start", {
@@ -180,8 +177,39 @@ export default function Game() {
                 setError(error as Error);
             }
         }
-        startGame();        
-    }, [])
+
+        // Cancels the current game sessions by fetch request.
+        const cancelGameSession = async () => {
+            if (session !== null) {
+                try {
+                    const response = await fetch(apiURL + "/session/cancel", {
+                        mode: "cors",
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type":"application/json"
+                        },
+                        body: JSON.stringify({id: session._id})
+                    })
+
+                    if (response.status >= 400 && response.status !== 404) {
+                        throw new Error("Couldn't properly close game session");
+                    }
+
+                } catch (error) {
+                    setError(error as Error);                   
+                }                
+            }
+        }
+
+        // On component mount, request a session open
+        if(session===null) startGameSession();
+
+        // On component dismount request a session cancel.
+        return () => {
+            cancelGameSession();
+        }
+
+    },[session])
 
     // Bind the resizing of the screen
     useEffect(() => {
@@ -198,9 +226,6 @@ export default function Game() {
 
     }, []);
     
-    //
-    const boxSize = 16;
-
     return (
         error && <p> {error.message} </p> ||
         loading && <p>Loading...</p> ||
@@ -224,12 +249,10 @@ export default function Game() {
                 </li>
             </ul>
             <div className="text-5xl relative">
-                    {gameState === "ongoing" && <Timer/>
-                        ||
-                        <SaveUsername score={score} handlePlayAgain={() => navigate("/")}/>
-                    }
-
-                    
+                {
+                    gameState === "ongoing" && <Timer />||
+                    gameState === "end" && <SaveUsername score={score} handlePlayAgain={() => navigate("/")}/>
+                }                    
             </div>
             <figure ref={imageRef} onClick={e => handleSelectClick(e, "image")} className="w-full h-auto border game-image relative">
                 {showMarker && <Circle position={markerPosition} foundCharacters={foundCharacters} handleClick={handleSelectClick} />}
